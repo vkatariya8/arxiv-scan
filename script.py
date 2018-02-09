14de2d8b84ba20d6d45563d59e4956fa1ad670d3
Vishal Katariya <vkatariya711@gmail.com>
2018-02-09 10:24:04 -0600

    Added journal ref part

--- script.py
+++ script.py
@@ -13,7 +13,6 @@ def download_file(download_url, title):
 	file = open(download_path + title.replace("\"", "") + ".pdf", 'wb')
 	file.write(response.read())
 	file.close()
-
 	print("Download Completed")
 
 url = "https://arxiv.org/list/quant-ph/new"
@@ -45,7 +44,7 @@ title_divs = soup.find_all('div', {'class': 'list-title mathjax'})
 all_author_divs = soup.find_all('div', {'class' : 'list-authors'})
 comment_divs = soup.find_all('div', {'class' : 'list-comments'})
 subject_divs = soup.find_all('div', {'class' : 'list-subjects'})
-
+journal_divs = soup.find_all('div', {'class' : 'list-journal-ref'})
 abstract_elements = soup.find_all('p')
 
 print "Total number of papers:", len(abstract_elements), ", ", len(link_elements)
@@ -53,28 +52,46 @@ print "Total number of papers:", len(abstract_elements), ", ", len(link_elements
 #new submissions and cross-lists
 i = 0
 c = 0 #comment_count
-while i < len(abstract_elements):
+journal_count = 0
+journal_check = 0
+comment_check = 0 
+while i < len(title_divs):
 	pdf_url = base_url + link_elements[i].find_all('a')[2].get('href')
 	title = title_divs[i].text[8:].replace("\n", "")
 	authors_div = all_author_divs[i].find_all('a')
 	for j in range(len(authors_div)):
 		authors[j] = authors_div[j].text
-	abstract = abstract_elements[i].text.replace("\n", " ")
+	if i < len(abstract_elements):
+		abstract = abstract_elements[i].text.replace("\n", " ")
 	subjects = subject_divs[i].text.replace("\n", " ")
-	if all_author_divs[i].find_next('div').text[:5] == "\nSubj":
+	if all_author_divs[i].find_next('div').text[:5] == "\nComm":
 		comments = comment_divs[c].text.replace("\n", " ")
 		c = c + 1
+		comment_check = 1
+	else:
+		comment_check = 0
+	if comment_check == 1:
+		if comment_divs[c-1].find_next('div').text[:5] == "\nJour":
+			journal = journal_divs[journal_count].text[14:]
+			journal_count = journal_count + 1
+			journal_check = 1
+		else:
+			journal_check = 0
 	print i + 1
 	print "\n\nTitle:   ", title
 	print "Authors: ", ", ".join(x for x in authors[:(j+1)])
 	print subjects[1:]
-	print comments[1:]
+	if comment_check == 1:
+		print comments[1:]
+		if journal_check == 1:
+			print "Journal: ", journal
 	print "\n"
-	print abstract + "\n"
-	#choice = raw_input('1/0\n')
+	if i < len(abstract_elements):
+		print abstract + "\n"
 	print("a/s/d")
 	choice = myGetch()
 	if choice == 's':
+		print "Downloading..."
 		download_file(pdf_url, title)
 		tiddler_subtext = add_tiddler_subtext(title,pdf_url)
 		for y in range(len(tiddler_subtext)):
@@ -88,40 +105,3 @@ while i < len(abstract_elements):
 		i = i - 1
 		continue
 	i = i + 1
-
-#replacements
-#for k in range(i, len(title_divs)):
-while i < len(title_divs):
-	pdf_url = base_url + link_elements[i].find_all('a')[2].get('href')
-	title = title_divs[i].text[8:].replace("\n", "")
-	subjects = subject_divs[i].text.replace("\n", " ")
-	authors_div = all_author_divs[i].find_all('a')
-	if all_author_divs[i].find_next('div').text[:5] == "\nSubj":
-		comments = comment_divs[c].text.replace("\n", " ")
-		c = c + 1
-	for j in range(len(authors_div)):
-		authors[j] = authors_div[j].text
-	print i + 1
-	print "\n\nTitle:   ", title
-	print "Authors: ", ", ".join(x for x in authors[:(j+1)])
-	print subjects[1:]
-	print comments[1:]
-	#choice = raw_input('1/0?')
-	print("a/s/d")
-	choice = myGetch()
-	if choice == "s":
-		download_file(pdf_url, title)
-		tiddler_subtext = add_tiddler_subtext(title,pdf_url)
-		for y in range(len(tiddler_subtext)):
-			try:
-				tiddler_file.write(tiddler_subtext[y])
-				tiddler_file.write("\n")
-			except:
-				continue
-	unused_variable = os.system("clear")
-	if choice == "a":
-		i = i - 1
-		continue
-	i = i + 1
-datefile = open('date.txt', 'w')
-datefile.write(date)
